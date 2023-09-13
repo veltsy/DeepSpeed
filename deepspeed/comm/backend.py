a9b0ade8bba633704f7950a580e4ed83957ce9a9
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2023-09-13 00:43:44 -0400

    Update backend.py

--- deepspeed/comm/backend.py
+++ deepspeed/comm/backend.py
@@ -24,7 +24,7 @@ In the future, directly use NCCL/MPI/Gloo/etc without requiring torch.distribute
 
 class Backend(object):
 
-    def __init__(self, name='backend', rank=0, size=1):
+    def __init__(self, name='backend', rank=0, size=int(os.getenv('WORLD_SIZE', '1'))):
         self.name = name
         # The world size and rank of the world process group
         self.world_group = None
